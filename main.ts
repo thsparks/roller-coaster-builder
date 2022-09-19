579e28f4684ae00e19318eb2cdc605c54471cf5c
thsparks <69657545+thsparks@users.noreply.github.com>
2022-09-19 12:57:00 -0600

    Add powered straight line box

--- main.ts
+++ main.ts
@@ -4,16 +4,38 @@ namespace rollerCoasterBuilder {
 
     //% block="place rail at position $position"
     //% position.shadow=minecraftCreatePosition
-    //% blockId="rollerCoasterBuilderCreateRail"
-    export function CreateRail(position: Position) {
-        CreateRailInternal(position, railBase, RAIL)
+    //% blockId="rollerCoasterBuilderPlaceRail"
+    export function PlaceRail(position: Position) {
+        placeRailInternal(position, railBase, RAIL)
     }
 
     //% block="place powered rail at position $position"
     //% position.shadow=minecraftCreatePosition
-    //% blockId="rollerCoasterBuilderCreatePoweredRail"
-    export function CreatePoweredRail(position: Position) {
-        CreateRailInternal(position, REDSTONE_BLOCK, POWERED_RAIL)
+    //% blockId="rollerCoasterBuilderPlacePoweredRail"
+    export function placePoweredRail(position: Position) {
+        placeRailInternal(position, REDSTONE_BLOCK, POWERED_RAIL)
+    }
+
+    //% block="place powered straight line starting at $position of length $length"
+    //% position.shadow=minecraftCreatePosition
+    //% length.defl=9 length.min=1 length.max=17
+    //% blockId="rollerCoasterBuilderPlacePoweredLine"
+    export function placePoweredLine(position: Position, length: number) {
+        builder.teleportTo(posCamera(0, -1, 1))
+        builder.face(positions.toCompassDirection(player.getOrientation()))
+        for (let index = 0; index <= length; index++) {
+            if (index == Math.floor(length / 2)) {
+                rollerCoasterBuilder.placePoweredRail(builder.position())
+            } else {
+                placeRailInternal(builder.position(), railBase, POWERED_RAIL)
+            }
+            builder.move(FORWARD, 1)
+        }
+    }
+
+    function placeRailInternal(position: Position, baseBlock: number, railBlock: number) {
+        blocks.place(baseBlock, position)
+        blocks.place(railBlock, position.move(CardinalDirection.Up, 1))
     }
 
     //% group="Customization"
@@ -22,10 +44,5 @@ namespace rollerCoasterBuilder {
     //% blockId="rollerCoasterBuilderSetBaseBlock"
     export function SetRollerCoasterBaseBlock(blockType: number) {
         railBase = blockType
-    }
-
-    function CreateRailInternal(position: Position, baseBlock: number, railBlock: number) {
-        blocks.place(baseBlock, position)
-        blocks.place(railBlock, position.move(CardinalDirection.Up, 1))
     }
 }
