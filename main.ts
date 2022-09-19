db8b3c7f35a3cc5a5a8f4f12986c8eb5f65de597
thsparks <69657545+thsparks@users.noreply.github.com>
2022-09-19 11:35:43 -0600

    Some initial blocks

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
-function CreatePoweredRail (position: Position) {
-    CreateRailInternal(position, REDSTONE_BLOCK, POWERED_RAIL)
+//% color="#AA278D" weight=100 block="Roller Coaster"
+namespace rollerCoasterBuilder {
+    let railBase = PLANKS_OAK
+
+    //% block
+    export function CreatePoweredRail(position: Position) {
+        CreateRailInternal(position, REDSTONE_BLOCK, POWERED_RAIL)
+    }
+
+    //% block
+    export function CreateRail(position: Position) {
+        CreateRailInternal(position, railBase, RAIL)
+    }
+
+    //% block
+    //% blockType.shadow=minecraftBlock
+    export function SetRollerCoasterBaseBlock(blockType: number) {
+        railBase = blockType
+    }
+
+    function CreateRailInternal(position: Position, baseBlock: number, railBlock: number) {
+        blocks.place(baseBlock, position)
+        blocks.place(railBlock, position.move(CardinalDirection.Up, 1))
+    }
 }
-function CreateRailInternal (position: Position, baseBlock: number, railBlock: number) {
-    blocks.place(baseBlock, position)
-    blocks.place(railBlock, position.move(CardinalDirection.Up, 1))
-}
-function CreateRail (position: Position) {
-    CreateRailInternal(position, RailBase, RAIL)
-}
-let RailBase = 0
-RailBase = PLANKS_OAK
